5db340232218a5f6a977f176fe87ec9bb96d653b
morganjones4 <morganjones4@mail.usf.edu>
2019-08-29 16:26:18 -0400

    Add project files.

---
+++ ISM4300THISISMYFIRSTAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM4300THISISMYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
